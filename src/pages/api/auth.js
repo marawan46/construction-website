0a629742a25a18f649b56e4dd3b9ad91622b02d2
miroElbehery <miroelbehery178@gmail.com>
2026-02-25 10:20:57 +0200

    fix OAuth api endpoints

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
-  const client_id = process.env.GITHUB_CLIENT_ID;
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user`);
-}
+export const GET = async ({ redirect }) => {
+  const client_id = import.meta.env.GITHUB_CLIENT_ID;
+  return redirect(`https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user`);
+};
